976020a2f22033d4be3d4672c032dc7e545b97e3
hayoola <onaolapo.ayo@gmail.com>
2022-08-13 21:34:36 +0400

    Config app to use swagger for documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Document convert')
+    .setDescription('This is the document convert application')
+    .setVersion('1.0')
+    .addTag('Document converter')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
